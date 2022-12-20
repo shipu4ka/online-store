7c84195a82d2f2a873b3e439a88456b4e8959d6a
Elena <myone72@yahoo.com>
2022-12-20 13:36:00 -0500

    feat: add cart total info block into header

--- src/models/components/header.ts
+++ src/models/components/header.ts
@@ -17,12 +17,19 @@ const Navigation = [
   {
     id: PageIds.CartPage,
     text: '<img src="../../assets/icons/cart_empty.svg" alt="Cart">',
-  }
-]
+  },
+];
 
 class Header extends Component {
   constructor(className: string) {
     super(className);
+  }
+
+  createNavElement(item: number) {
+    const elem = this.createComponentBlock('a', 'header__link') as HTMLLinkElement;
+    elem.href = `#${Navigation[item].id}`;
+    elem.innerHTML = Navigation[item].text;
+    return elem;
   }
 
   renderPageNav() {
@@ -30,19 +37,24 @@ class Header extends Component {
     pageNav.classList.add('header__nav');
     pageNav.classList.add('container');
 
-    Navigation.forEach((item) => {
-      const navItem = document.createElement('a');
-      navItem.classList.add('header__link');
-      navItem.href = `#${item.id}`;
-      navItem.innerHTML = item.text;
-      pageNav.append(navItem);
-    });
+    const logo = this.createNavElement(0);
+    const description = this.createNavElement(1);
+    const confirm = this.createNavElement(2);
+    const cart = this.createNavElement(3);
 
-    this.element.append(pageNav);
+    const sumBlock = this.createComponentBlock('div', 'header__sum');
+    const sumText = this.createComponentBlock('span', 'header__sum-text');
+    const sum = this.createComponentBlock('span', 'header__sum-total');
+    sumText.textContent = 'Cart total: $';
+    sum.textContent = '0.00';
+    sumBlock.append(...[sumText, sum]);
 
     const total = this.createComponentBlock('div', 'header__total');
     total.textContent = '0';
-    pageNav.append(total);
+
+    pageNav.append(...[logo, sumBlock, description, confirm, cart, total]);
+
+    this.element.append(pageNav);
   }
 
   render() {
